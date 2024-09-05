fcb8982199f73baf3e51004441037378a414ef98
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2024-09-04 21:36:56 -0400

    Initial Commit

---
+++ MyFetchChallenge/MyFetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFetchChallengeApp.swift
+//  MyFetchChallenge
+//
+//  Created by ENMANUEL TORRES on 4/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
